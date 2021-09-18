c9eed7f25d58b4d079118904752f78259a222a3a
Greg Laurent <gregory.m.laurent@gmail.com>
2021-09-18 09:01:05 -0700

    fixed calculation, clean up, added docs, added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,43 @@
+use fxhash::FxHasher;
 use bit_vec::BitVec;
-use std::{
-    collections::hash_map::{DefaultHasher, RandomState}, 
-    hash::{BuildHasher, Hash, Hasher}, 
-    marker::PhantomData};
+use std::{hash::{Hash, Hasher}, marker::PhantomData};
 
-const FALSE_POS_PROB: f64 = -1.0f64;
-const LN_2_SQR: f64 = core::f64::consts::LN_2 * core::f64::consts::LN_2;
+const FALSE_POS_PROB: f64 = -1.0;
+const LN_2: f64 = core::f64::consts::LN_2;
+const LN_2_SQR: f64 = LN_2 * LN_2;
 
-pub struct BloomFilter<T: ?Sized + Send + Sync> {
-    k: u64,
+/// A standard Bloom Filter, using Kirsch and Mitzenmacher
+/// (two hash functions) and Fx Hash.
+/// See: [FX Hash](https://github.com/cbreeden/fxhash)
+pub struct BloomFilter<T: ?Sized> {
+    k: u64, 
     m: usize,
-    hashers: [DefaultHasher; 2],
+    hashers: [FxHasher; 2],
     bitmap: BitVec,
     _phantom: PhantomData<T>,
 }
 
-impl<T: ?Sized + Send + Sync> BloomFilter<T> { 
+impl<T: ?Sized> BloomFilter<T> { 
+    /// Returns a BloomFilter with an optimized k and m 
+    /// 
+    /// # Arguments
+    ///
+    /// * 'size' - A usize that sets the size of the filter
+    /// * 'false_pos_rate' - The acceptable false positive rate
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tulip::BloomFilter;
+    /// let filter: BloomFilter<usize> = BloomFilter::new(1_000, 0.001);
+    /// ```
     pub fn new(size: usize, false_pos_rate: f64) -> Self { 
         let k = Self::optimal_k(false_pos_rate);
-        let m = Self::calc_bitmap(size, false_pos_rate);
-        let bitmap = BitVec::from_elem(m as usize, false);
+        let m = Self::optimal_m(false_pos_rate, size);
+        let bitmap = BitVec::from_elem(m, false);
         let hashers = [ 
-            RandomState::new().build_hasher(),
-            RandomState::new().build_hasher(),
+            FxHasher::default(),
+            FxHasher::default(),
         ];
 
         BloomFilter { 
@@ -33,44 +48,82 @@ impl<T: ?Sized + Send + Sync> BloomFilter<T> {
             _phantom: PhantomData
         }
     }
-
-    fn calc_bitmap(size: usize, false_pos_rate: f64) -> usize { 
+ 
+    /// Calculate optimal m value for the filter 
+    /// where m is the optimal number of bits in the bit array
+    /// while preventing overfill
+    ///
+    /// where P is the probability of false positives
+    /// and n is the acceptable false postive rate
+    /// k = -( n * lnP / (ln2)^2 )
+    fn optimal_m(false_pos_rate: f64, size: usize) -> usize { 
         ((size as f64 * FALSE_POS_PROB * false_pos_rate.ln()) / LN_2_SQR).ceil() as usize
     }
-    
+
+    /// Calculate optimal k value for the filter 
+    /// where k is the number of functions to hash input T
+    /// yielding k indices into the bit array
+    ///
+    /// where P is the probability of false positives
+    /// k = -( lnP / ln2 )
     fn optimal_k(false_pos_rate: f64) -> u64 {
-        (false_pos_rate.ln() * FALSE_POS_PROB / LN_2_SQR).ceil() as u64
+        (false_pos_rate.ln() * FALSE_POS_PROB / LN_2).ceil() as u64
     }
 
-    fn hash(&self, item: &T) -> (u64, u64) where T: Hash{ 
+    /// Hash values T for Bloomfilter
+    fn hash(&self, t: &T) -> (u64, u64) where T: Hash{ 
         let hash1 = &mut self.hashers[0].clone();
         let hash2 = &mut self.hashers[1].clone();
 
-        item.hash(hash1);
-        item.hash(hash2);
+        t.hash(hash1);
+        t.hash(hash2);
 
         (hash1.finish(), hash2.finish())
     }
 
-    fn get_index(&self, hash1: u64, hash2: u64, i: u64) -> usize { 
+    /// Retrieve the index of indexes by simulating 
+    /// more than 2 hashers
+    ///
+    /// Prevent Overflow:
+    /// wrapping_add: wrapping add around at the boundary type
+    /// wrapping_mul: wrapping mult around at the boundary type
+    fn find_index(&self, i: u64, hash1: u64, hash2: u64) -> usize { 
         hash1.wrapping_add((i).wrapping_mul(hash2)) as usize % self.m
     }
 
-    pub fn insert(&mut self, item: &T) where T: Hash {
-        let (h1, h2) = self.hash(item);
+    /// Insert T into the BloomFilter index
+    ///
+    /// # Examples: 
+    /// ```
+    /// use tulip::BloomFilter;
+    /// let mut bf = BloomFilter::new(1000, 0.0001);
+    /// bf.insert(&10);
+    /// assert!(bf.contains(&10));
+    /// ```
+    pub fn insert(&mut self, t: &T) where T: Hash {
+        let (hash1, hash2) = self.hash(t);
 
         for i in 0..self.k { 
-            let index = self.get_index(h1, h2, i as u64);
-
+            let index = self.find_index(i, hash1, hash2);
             self.bitmap.set(index, true);
         }
     }
 
-    pub fn contains(&mut self, item: &T) -> bool where T: Hash {
-        let (h1, h2) = self.hash(item);
+    /// Check if t of type T into the BloomFilter index
+    ///
+    /// # Examples: 
+    /// ```
+    /// use tulip::BloomFilter;
+    /// let mut bf = BloomFilter::new(1000, 0.0001);
+    /// assert!(!bf.contains(&10));
+    /// bf.insert(&10);
+    /// assert!(bf.contains(&10));
+    /// ```
+    pub fn contains(&mut self, t: &T) -> bool where T: Hash {
+        let (hash1, hash2) = self.hash(t);
 
         for i in 0..self.k { 
-            let index = self.get_index(h1, h2, i as u64);
+            let index = self.find_index(i, hash1, hash2);
             if !self.bitmap.get(index).unwrap() { 
                 return false
             }
@@ -84,18 +137,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_insert() {
-        let mut bf = BloomFilter::new(1000, 0.0000001);
-        bf.insert(&10);
-        assert!(bf.contains(&10));
+    fn test_optimal_m() { 
+        let o = BloomFilter::<u16>::optimal_m(0.001, 1000);
+        assert_eq!(o, 14378);
     }
 
     #[test]
-    fn test_check_and_insert() {
-        let mut bf = BloomFilter::new(1000, 0.01);
+    fn test_optimal_k() { 
+        let o = BloomFilter::<u16>::optimal_k(0.001);
+        assert_eq!(o, 10);
+    }
 
-        assert!(!bf.contains("test2"));
-        bf.insert("test2");
-        assert!(bf.contains("test2"));
+    #[test]
+    fn test_hash() { 
+        let bf = BloomFilter::new(1000, 0.0001);
+        let o = bf.hash("test");
+        assert_eq!(o, (14825577416135075223, 14825577416135075223));
     }
 }
